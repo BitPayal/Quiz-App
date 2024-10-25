21878ee5f56779866f9b36f20659f647289838cc
BitPayal <gury20112003@gmail.com>
2024-10-25 21:55:55 +0530

    files added

---
+++ src/assets/data.js
@@ -0,0 +1,103 @@
+export const data = [
+    {
+      question: 'What is the capital of France?',
+      
+        option1: 'Paris',
+        option2: 'London',
+        option3: 'Berlin',
+        option4: 'Madrid',
+    
+      ans: 1,
+    },
+    {
+      question: 'What is the largest planet in our solar system?',
+      
+        option1: 'Mars',
+        option2: 'Saturn',
+        option3: 'Jupiter',
+        option4: 'Neptune',
+      
+      ans: 3,
+    },
+    {
+      question: 'Which country won the FIFA World Cup in 2018?',
+      
+        option1: 'Brazil',
+        option2: 'Germany',
+        option3: 'France',
+        option4: 'Argentina',
+    
+      ans: 3,
+    },
+    {
+      question: 'What is the tallest mountain in the world?',
+      
+        option1: 'Mount Everest',
+        option2: 'K2',
+        option3: 'Kangchenjunga',
+        option4: 'Makalu',
+      
+      ans: 1,
+    },
+    {
+      question: 'Which is the largest ocean on Earth?',
+    
+        option1: 'Pacific Ocean',
+        option2: 'Indian Ocean',
+        option3: 'Atlantic Ocean',
+        option4: 'Arctic Ocean',
+      
+      ans: 1,
+    },
+    {
+      question: 'What is the chemical symbol for gold?',
+  
+        option1: 'Au',
+        option2: 'Ag',
+        option3: 'Cu',
+        option4: 'Fe',
+      
+      ans: 1,
+    },
+    {
+      question: 'Who painted the Mona Lisa?',
+      
+        option1: 'Pablo Picasso',
+        option2: 'Vincent van Gogh',
+        option3: 'Leonardo da Vinci',
+        option4: 'Michelangelo',
+   
+      ans: 3,
+    },
+    {
+      question: 'Which planet is known as the Red Planet?',
+     
+        option1: 'Mars',
+        option2: 'Venus',
+        option3: 'Mercury',
+        option4: 'Uranus',
+      
+      ans: 1,
+    },
+    {
+      question: 'What is the largest species of shark?',
+ 
+        option1: 'Great White Shark',
+        option2: 'Whale Shark',
+        option3: 'Tiger Shark',
+        option4: 'Hammerhead Shark',
+
+      ans: 2,
+    },
+    {
+      question: 'Which animal is known as the King of the Jungle?',
+     
+        option1: 'Lion',
+        option2: 'Tiger',
+        option3: 'Elephant',
+        option4: 'Giraffe',
+      
+      ans: 1,
+    },
+  ];
+  
